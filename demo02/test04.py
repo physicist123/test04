6d05fdf4fd6c92a45a3f2345d23c205454bf3710
Alex <1429377449@qq.com>
2019-10-26 17:35:25 +0800

    commit

---
+++ demo02/test04.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+# @Time : 2019/10/26 16:53 
+# @Author : Alex
+# @Site :  
+# @File : test04.py 
+# @Software: PyCharm
+
+def transform(a,b):
+    return a+b
+
+if __name__ == '__main__':
+    sub=transform(10,20)
+    print(sub)
