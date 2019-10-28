b5467a96a5671545084975a545bfb5f96641e3fe
Alex <1429377449@qq.com>
2019-10-28 11:07:40 +0800

    tuple test01

---
+++ day09/tupleTest.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+# @Time : 2019/10/28 10:58 
+# @Author : Alex
+# @Site :  
+# @File : tupleTest.py 
+# @Software: PyCharm
+
+mixed_tuple=(2,)
+print(mixed_tuple)
